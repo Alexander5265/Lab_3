d7484c90469a8636ae8afe2a6a714b4191399573
Alexander5265 <104996703+Alexander5265@users.noreply.github.com>
2026-03-16 14:31:25 +0500

    Update Program.fs

--- 3_2/3_2/Program.fs
+++ 3_2/3_2/Program.fs
@@ -1,26 +1,27 @@
-﻿open System
+open System
 
 // функция перевода двоичного числа в десятичное
 let binToDec (binary: string) =
     Convert.ToInt32(binary, 2)
 
 // функция для вычисления суммы с помощью Seq.fold
-let sumBinaryNumbers (numbers: string list) =
+let sumBinaryNumbers (numbers: seq<string>) =
     numbers
     |> Seq.fold (fun sum element ->
-        sum + binToDec element) 
+        sum + binToDec element)
         0
-
 
 [<EntryPoint>]
 let main argv =
 
-    printfn "Исходный двоичный список через пробел: "
+    printfn "Исходная двоичная последовательность через пробел:"
     let input1 = Console.ReadLine()
-    let list1 = input1.Split(' ') |> Array.toList
-        
-    let result = sumBinaryNumbers list1
 
+    let seq1 =
+        input1.Split(' ')
+        |> Seq.map id
+
+    let result = sumBinaryNumbers seq1
     printfn "Сумма = %d" result
 
     0
